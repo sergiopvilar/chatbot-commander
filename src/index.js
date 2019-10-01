e6eb513577dd5b1e5ffc59e59b27524608653fb3
Sergio Vilar <sergiovilar.r@gmail.com>
2019-10-01 00:46:19 -0300

    Add method to handle command strings

--- src/index.js
+++ src/index.js
@@ -1,8 +1,49 @@
 class Commander {
 
-  constructor(prefix) {
+  constructor(prefix, options) {
     this.prefix = prefix
     this.commands = []
   }
 
+  handle(string, attributes = {}) {
+    for (var i in this.commands) {
+      cmd = this.commands[i]
+      if (
+        cmd.input && message.content.startsWith(`${cmd.prefix}${cmd.command}`) ||
+        !cmd.input & message.content.trim() == `${cmd.prefix}${cmd.command}`
+      ) {
+        cmd.handler(string.replace(`${cmd.prefix}${cmd.command}`, '').trim(), attributes)
+        return
+      }
+    }
+  }
+
+  register(options, handler) {
+    let { command, input, prefix } = options
+
+    if(typeof handler !== 'function') throw new Error('Handler argument must be a function')
+    if(typeof command === 'undefined') throw new Error('command option is required')
+    if (typeof prefix === 'undefined') prefix = this.prefix
+    if (typeof input === 'undefined') input = true
+
+    if(options.mention && command !== '') {
+      prefix = this.options.username
+      command = ` ${command}`
+      input = true
+    } else if (options.mention && command === '') {
+      prefix = ''
+      command = this.options.username
+      input = true
+    }
+
+    this.commands.push({
+      command: command,
+      handler: handler,
+      input: input,
+      prefix: prefix
+    })
+  }
+
 }
+
+module.exports = Commander
