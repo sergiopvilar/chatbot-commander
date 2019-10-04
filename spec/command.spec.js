56f454190a5254e0abb750c7051ead168e388440
Sergio Vilar <sergiovilar.r@gmail.com>
2019-10-04 11:41:58 -0300

    Adds a linter

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -1,138 +1,138 @@
-import assert from 'assert'
-import Commander from '../src/index.js'
+import assert from 'assert';
+import Commander from '../src/index';
 
 describe('Commander', () => {
 
   describe('Throwing Errors', () => {
     it('should throw an error if a command is not specified', () => {
-      let commander = new Commander()
+      const commander = new Commander();
 
-      assert.throws(() => commander.register({}, () => {}), Error, 'Command option is required');
-    })
+      assert.throws(() => commander.register({}, (attr) => attr), Error, 'Command option is required');
+    });
 
     it('should throw an error if a handler is not specified', () => {
-      let commander = new Commander()
+      const commander = new Commander();
 
       assert.throws(() => commander.register({
-        command: 'test'
+        command: 'test',
       }), Error, 'Handler argument must be a function');
-    })
+    });
 
     it('should throw an error if a handler is not a function', () => {
-      let commander = new Commander()
+      const commander = new Commander();
 
       assert.throws(() => commander.register({
         command: 'test',
       }, {}), Error, 'Handler argument must be a function');
-    })
-  })
+    });
+  });
 
   describe('Handling Inputs', () => {
     it('should call handler if input is specified', (done) => {
-      let commander = new Commander(),
-        commandInput = 'this is a simple test',
-        commandAttr = {
-          foo: 'bar'
-        }
-
-      commander.register({
-        command: 'test'
-      }, (input, attrs) => {
-        assert.equal(input, commandInput)
-        assert.equal(attrs, commandAttr)
-        done()
-      })
-      commander.handle(`!test ${commandInput}`, commandAttr)
-    })
-
-    it('should call handler if input is not specified', () => {
-      let commander = new Commander(),
-        commandAttr = {
-          foo: 'bar'
-        }
-
-      commander.register({
-        command: 'test'
-      }, (input, attrs) => {
-        assert.equal(input, '')
-        assert.equal(attrs, commandAttr)
-        done()
-      })
-      commander.handle('!test', commandAttr)
-    })
-
-    it('should not call handler if input is not specified', () => {
-      let commander = new Commander()
-
-      commander.register({
-        command: 'test'
-      }, (input, attrs) => {
-        throw new Error('Must not call handler')
-      })
-      commander.handle('!test')
-    }).timeout(1000)
-
-    it('should not call handler if input proviced but input options is set to false', () => {
-      let commander = new Commander()
+      const commander = new Commander();
+      const commandInput = 'this is a simple test';
+      const commandAttr = {
+        foo: 'bar',
+      };
 
       commander.register({
         command: 'test',
-        input: false
       }, (input, attrs) => {
-        throw new Error('Must not call handler')
-      })
+        assert.equal(input, commandInput);
+        assert.equal(attrs, commandAttr);
+        done();
+      });
+      commander.handle(`!test ${commandInput}`, commandAttr);
+    });
 
-      commander.handle('!test argument')
-    }).timeout(1000)
-  })
+    it('should call handler if input is not specified', (done) => {
+      const commander = new Commander();
+      const commandAttr = {
+        foo: 'bar',
+      };
+
+      commander.register({
+        command: 'test',
+      }, (input, attrs) => {
+        assert.equal(input, '');
+        assert.equal(attrs, commandAttr);
+        done();
+      });
+      commander.handle('!test', commandAttr);
+    });
+
+    it('should not call handler if input is not specified', () => {
+      const commander = new Commander();
+
+      commander.register({
+        command: 'test',
+      }, () => {
+        throw new Error('Must not call handler');
+      });
+      commander.handle('!test');
+    }).timeout(1000);
+
+    it('should not call handler if input proviced but input options is set to false', () => {
+      const commander = new Commander();
+
+      commander.register({
+        command: 'test',
+        input: false,
+      }, () => {
+        throw new Error('Must not call handler');
+      });
+
+      commander.handle('!test argument');
+    }).timeout(1000);
+  });
 
   describe('Handling Prefixes', () => {
     it('should handle custom prefixes', (done) => {
-      let commander = new Commander('?')
+      const commander = new Commander('?');
 
       commander.register({
         command: 'test',
-        input: false
-      }, (input, attrs) => {
-        assert.equal(input, '')
-        done()
-      })
+        input: false,
+      }, (input) => {
+        assert.equal(input, '');
+        done();
+      });
 
-      commander.handle(`?test`)
-    })
+      commander.handle(`?test`);
+    });
 
     it('should call handler if prefix is empty and input and command are provided', (done) => {
-      let commander = new Commander(''),
-        commandInput = 'this is a simple test',
-        commandAttr = {
-          foo: 'bar'
-        }
-
-      commander.register({
-        command: 'test'
-      }, (input, attrs) => {
-        assert.equal(input, commandInput)
-        assert.equal(attrs, commandAttr)
-        done()
-      })
-      commander.handle(`test ${commandInput}`, commandAttr)
-    })
-
-    it('should call handler if prefix and input are empty but command is provided', (done) => {
-      let commander = new Commander(''),
-        commandAttr = {
-          foo: 'bar'
-        }
+      const commander = new Commander('');
+      const commandInput = 'this is a simple test';
+      const commandAttr = {
+        foo: 'bar',
+      };
 
       commander.register({
         command: 'test',
-        input: false
       }, (input, attrs) => {
-        assert.equal(attrs, commandAttr)
-        done()
-      })
-      commander.handle(`test`, commandAttr)
-    })
-  })
+        assert.equal(input, commandInput);
+        assert.equal(attrs, commandAttr);
+        done();
+      });
+      commander.handle(`test ${commandInput}`, commandAttr);
+    });
 
-})
+    it('should call handler if prefix and input are empty but command is provided', (done) => {
+      const commander = new Commander('');
+      const commandAttr = {
+        foo: 'bar',
+      };
+
+      commander.register({
+        command: 'test',
+        input: false,
+      }, (input, attrs) => {
+        assert.equal(attrs, commandAttr);
+        done();
+      });
+      commander.handle(`test`, commandAttr);
+    });
+  });
+
+});
--- src/index.js
+++ src/index.js
@@ -1,51 +1,52 @@
 class Commander {
-
-  constructor(prefix = '!', options) {
-    this.prefix = prefix
-    this.commands = []
+  constructor(prefix = '!') {
+    this.prefix = prefix;
+    this.commands = [];
   }
 
   handle(string, attributes = {}) {
-    let cmd
-
-    for (var i in this.commands) {
-      cmd = this.commands[i]
+    this.commands.forEach((cmd) => {
       if (
-        cmd.input && string.startsWith(`${cmd.prefix}${cmd.command}`) && string !== `${cmd.prefix}${cmd.command}` ||
-        !cmd.input & string.trim() == `${cmd.prefix}${cmd.command}`
+        (cmd.input && string.startsWith(`${cmd.prefix}${cmd.command}`) && string !== `${cmd.prefix}${cmd.command}`) ||
+        (!cmd.input & string.trim() === `${cmd.prefix}${cmd.command}`)
       ) {
-        cmd.handler(string.replace(`${cmd.prefix}${cmd.command}`, '').trim(), attributes)
-        return
+        cmd.handler(string.replace(`${cmd.prefix}${cmd.command}`, '').trim(), attributes);
       }
-    }
+    });
   }
 
   register(options, handler) {
-    let { command, input, prefix } = options
+    let {command, input, prefix} = options;
 
-    if(typeof handler !== 'function') throw new Error('Handler argument must be a function')
-    if(typeof command === 'undefined') throw new Error('Command option is required')
-    if (typeof prefix === 'undefined') prefix = this.prefix
-    if (typeof input === 'undefined') input = true
-
-    if(options.mention && command !== '') {
-      prefix = this.options.username
-      command = ` ${command}`
-      input = true
-    } else if (options.mention && command === '') {
-      prefix = ''
-      command = this.options.username
-      input = true
+    if (typeof handler !== 'function') {
+      throw new Error('Handler argument must be a function');
     }
 
-    this.commands.push({
-      command: command,
-      handler: handler,
-      input: input,
-      prefix: prefix
-    })
+    if (typeof command === 'undefined') {
+      throw new Error('Command option is required');
+    }
+
+    if (typeof prefix === 'undefined') {
+      prefix = this.prefix;
+    }
+
+    if (typeof input === 'undefined') {
+      input = true;
+    }
+
+    if (options.mention && command !== '') {
+      prefix = this.options.username;
+      command = ` ${command}`;
+      input = true;
+    } else if (options.mention && command === '') {
+      prefix = '';
+      command = this.options.username;
+      input = true;
+    }
+
+    this.commands.push({command, handler, input, prefix});
   }
 
 }
 
-module.exports = Commander
+export default Commander;
